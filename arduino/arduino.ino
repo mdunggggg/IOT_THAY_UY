1d7ec06eec62a1d8853400f4b11725538fc31819
imdung <dungmanh74@gmail.com>
2024-08-20 15:00:33 +0700

    IOT: refactor code

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -2,41 +2,68 @@
 #include <PubSubClient.h>
 #include "DHT.h"
 
+#define Log(X) Serial.println("=========>>>>> " + String(X))
+#define LogI(X) Serial.print("=====>>>>> " + String(X))
+
 // WiFi settings
-const char *ssid = "Ahii"; // Enter your WiFi name
+const char *wifiName = "Ahii"; // Enter your WiFi name
 const char *password = "25072003";  // Enter WiFi password
 
 // MQTT Broker settings
-const char *mqtt_broker = "172.20.10.2"; // Enter your MQTT broker IP
-const char *topic = "topic/temp-humidity";
+const char *mosquittoServer = "172.20.10.2"; // Enter your MQTT broker IP
+const char *topic = "topic/temp-humidity-light";
 const int mqtt_port = 1883;
 
 WiFiClient espClient;
 PubSubClient client(espClient);
 
 // DHT Sensor settings
-#define DPIN 4        // Pin to connect DHT sensor (GPIO number, D2 on ESP8266)
-#define DTYPE DHT11   // Define DHT sensor type, DHT11 or DHT22
+#define DPIN 4       // D2-GPI04
+#define DTYPE DHT11  
 DHT dht(DPIN, DTYPE);
+
+// Light Sensor settings
+#define LIGHT 5  // D1-GPI05
 
 void setup() {
   // Set software serial baud to 115200
   Serial.begin(115200);
 
-  // Initialize DHT sensor
-  dht.begin();
+  setUpDHTSensor();
+
+  setUpLightSensor();
 
   // Connect to WiFi
-  WiFi.begin(ssid, password);
+  connectToWifi();
+
+
+  // Connect to the MQTT broker (Mosquitto)
+  connectToBroker();
+
+  // Subscribe to the topic
+  client.subscribe(topic);
+}
+
+void setUpDHTSensor() {
+  dht.begin();
+}
+
+void setUpLightSensor() {
+  pinMode(LIGHT, INPUT);
+}
+
+void connectToWifi() {
+  WiFi.begin(wifiName, password);
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
-    Serial.println("Connecting to WiFi...");
+    Log("Connecting to WiFi...");
   }
-  Serial.println("Connected to the WiFi network");
+  Log("Connected to the WiFi network successfully");
+}
 
-  // Connect to the MQTT broker
-  client.setServer(mqtt_broker, mqtt_port);
-  client.setCallback(callback);
+void connectToBroker() {
+  client.setServer(mosquittoServer, mqtt_port);
+ // client.setCallback(callback);
   
   while (!client.connected()) {
     String client_id = "B21DCCN268";
@@ -44,58 +71,59 @@ void setup() {
     Serial.printf("The client %s connects to the MQTT broker\n", client_id.c_str());
     
     if (client.connect(client_id.c_str())) {
-      Serial.println("MQTT broker connected");
+      Log("MQTT broker connected");
     } else {
-      Serial.print("Failed with state ");
-      Serial.print(client.state());
+      LogI("Failed with state ");
+      Log(client.state());
       delay(2000);
     }
   }
-  
-  // Subscribe to the topic
-  client.subscribe(topic);
 }
 
-void callback(char *topic, byte *payload, unsigned int length) {
-  Serial.print("Message arrived in topic: ");
-  Serial.println(topic);
-  Serial.print("Message: ");
+// void callback(char *topic, byte *payload, unsigned int length) {
+//   Serial.print("Message arrived in topic: ");
+//   Serial.println(topic);
+//   Serial.print("Message: ");
   
-  for (int i = 0; i < length; i++) {
-    Serial.print((char)payload[i]);
-  }
+//   for (int i = 0; i < length; i++) {
+//     Serial.print((char)payload[i]);
+//   }
   
-  Serial.println();
-  Serial.println(" - - - - - - - - - - - -");
-}
+//   Serial.println();
+//   Serial.println(" - - - - - - - - - - - -");
+// }
 
 void loop() {
+  
   client.loop();
 
   // Read temperature and humidity from DHT sensor
-  float tc = dht.readTemperature(false);  // Read temperature in Celsius
-  float tf = dht.readTemperature(true);   // Read temperature in Fahrenheit
-  float hu = dht.readHumidity();          // Read humidity
+  float tc = dht.readTemperature(false);  
+  float hu = dht.readHumidity();          
 
-  // Check if any reads failed and exit early (to try again).
-  if (isnan(tc) || isnan(tf) || isnan(hu)) {
-    Serial.println("Failed to read from DHT sensor!");
+
+  if (isnan(tc) || isnan(hu)) {
+    Log("Failed to read from DHT sensor!");
     return;
   }
 
+  float light = analogRead(LIGHT);
+
   // Create a JSON payload with the sensor data
   String payload = "{";
-  payload += "\"temperature_celsius\": ";
+  payload += "\"temprature\": ";
   payload += tc;
-  payload += ", \"temperature_fahrenheit\": ";
-  payload += tf;
   payload += ", \"humidity\": ";
   payload += hu;
+  payload += ", \"light\": ";
+  payload += light;
   payload += "}";
 
-  // Publish the payload to the MQTT topic
+  Serial.println(payload);
+
+
+ // Publish the payload to the MQTT topic
   client.publish(topic, payload.c_str());
 
-  // Wait 2 seconds before the next loop
-  delay(2000);
+  delay(5000);
 }
