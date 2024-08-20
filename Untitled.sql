85588950a9108ff8ae18089800feb186a12bb27d
imdung <dungmanh74@gmail.com>
2024-08-20 12:00:58 +0700

    FE: init proj

---
+++ Untitled.sql
@@ -0,0 +1,8 @@
+CREATE DATABASE  IF NOT EXISTS `iot_thay_uy`;
+USE `iot_thay_uy`;
+CREATE TABLE data_sensor (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    temperature DOUBLE NOT NULL,
+    humidity DOUBLE NOT NULL,
+    time BIGINT
+);
---
+++ home_tracking/android/app/src/main/kotlin/com/mdung/home_tracking/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.mdung.home_tracking
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
